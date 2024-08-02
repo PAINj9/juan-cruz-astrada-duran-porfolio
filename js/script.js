5bb0458c130a94da7a9b3dacca4f2840b3415048
PAINj9 <juancruzastradaduran80@gmail.com>
2024-08-02 18:00:44 +0200

    18:00

--- js/script.js
+++ js/script.js
@@ -1,53 +1,35 @@
-document.addEventListener('DOMContentLoaded', function() {
+
+
+
     // Rotating Text
     const rotatingText = document.querySelector('.rotating-text');
-    const textArray = [
-        'Frontend Developer',
-        'Backend Developer',
-        'Video Editor',
-        'Graphic Designer'
-    ];
-    let currentIndex = 0;
+    if (rotatingText) {
+        const textArray = [
+            'Frontend Developer',
+            'Backend Developer',
+            'Video Editor',
+            'Graphic Designer'
+        ];
+        let currentIndex = 0;
 
-    function changeText() {
-        const currentText = rotatingText.querySelector('.current');
-        const nextText = document.createElement('span');
-        nextText.className = 'next';
-        nextText.textContent = textArray[currentIndex];
-        rotatingText.appendChild(nextText);
+        function changeText() {
+            const currentText = rotatingText.querySelector('.current');
+            const nextText = document.createElement('span');
+            nextText.className = 'next';
+            nextText.textContent = textArray[currentIndex];
+            rotatingText.appendChild(nextText);
 
-        setTimeout(() => {
-            currentText.classList.remove('current');
-            nextText.classList.remove('next');
-            nextText.classList.add('current');
-            rotatingText.removeChild(currentText);
-        }, 1000); // Cambiado a 1000 para que coincida con el intervalo de cambio de texto
+            setTimeout(() => {
+                if (currentText) {
+                    currentText.classList.remove('current');
+                    rotatingText.removeChild(currentText);
+                }
+                nextText.classList.remove('next');
+                nextText.classList.add('current');
+            }, 1000); // Tiempo de transición de texto
 
-        currentIndex = (currentIndex + 1) % textArray.length;
+            currentIndex = (currentIndex + 1) % textArray.length;
+        }
+
+        setInterval(changeText, 2500); // Intervalo de cambio de texto
     }
-
-    setInterval(changeText, 2500);
-
-    // Fade-in Effect
-    const faders = document.querySelectorAll('.fade-in');
-
-    const appearOptions = {
-        threshold: 0,
-        rootMargin: '0px 0px -100px 0px'
-    };
-
-    const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
-        entries.forEach(entry => {
-            if (!entry.isIntersecting) {
-                return;
-            } else {
-                entry.target.classList.add('visible');
-                appearOnScroll.unobserve(entry.target);
-            }
-        });
-    }, appearOptions);
-
-    faders.forEach(fader => {
-        appearOnScroll.observe(fader);
-    });
-});
