5d30183eef5c755d7c706b54c1a8a596a32fb4a2
PAINj9 <juancruzastradaduran80@gmail.com>
2024-08-02 17:36:36 +0200

    02-08

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Rotating Text
     const rotatingText = document.querySelector('.rotating-text');
     const textArray = [
         'Frontend Developer',
@@ -20,10 +21,33 @@ document.addEventListener('DOMContentLoaded', function() {
             nextText.classList.remove('next');
             nextText.classList.add('current');
             rotatingText.removeChild(currentText);
-        }, 10);
+        }, 1000); // Cambiado a 1000 para que coincida con el intervalo de cambio de texto
 
         currentIndex = (currentIndex + 1) % textArray.length;
     }
 
     setInterval(changeText, 2500);
+
+    // Fade-in Effect
+    const faders = document.querySelectorAll('.fade-in');
+
+    const appearOptions = {
+        threshold: 0,
+        rootMargin: '0px 0px -100px 0px'
+    };
+
+    const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.classList.add('visible');
+                appearOnScroll.unobserve(entry.target);
+            }
+        });
+    }, appearOptions);
+
+    faders.forEach(fader => {
+        appearOnScroll.observe(fader);
+    });
 });
