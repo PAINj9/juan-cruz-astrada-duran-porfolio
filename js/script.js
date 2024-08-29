c1c945f854b2d0499b55444d3b772b57fb71ef41
PAINj9 <juancruzastradaduran80@gmail.com>
2024-08-29 10:01:34 +0200

    Almost Finished

--- js/script.js
+++ js/script.js
@@ -1,5 +1,96 @@
 
 
+//  ML12
+
+function applyML12Animation(element) {
+    element.innerHTML = element.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+    anime.timeline({ loop: true })
+        .add({
+            targets: element.querySelectorAll('.letter'),
+            translateX: [40, 0],
+            opacity: [0, 1],
+            easing: "easeOutExpo",
+            duration: 1200,
+            delay: (el, i) => 500 + 30 * i
+        }).add({
+            targets: element.querySelectorAll('.letter'),
+            translateX: [0, -30],
+            opacity: [1, 0],
+            easing: "easeInExpo",
+            duration: 4100,
+            delay: (el, i) => 100 + 30 * i
+        });
+}
+
+function applyML12ToAll() {
+    const elements = document.querySelectorAll('.ml12');
+    elements.forEach(element => {
+        applyML12Animation(element);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    applyML12ToAll();
+});
+
+
+// ML 11
+var textWrapper = document.querySelector('.ml11 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml11 .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700
+  })
+  .add({
+    targets: '.ml11 .line',
+    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+    easing: "easeOutExpo",
+    duration: 700,
+    delay: 100
+  }).add({
+    targets: '.ml11 .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 2600,
+    offset: '-=775',
+    delay: (el, i) => 34 * (i+1)
+  }).add({
+    targets: '.ml11',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+//ML17
+
+
+var textWrapper = document.querySelector('.ml7 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml7 .letter',
+    translateY: ["1.1em", 0],
+    translateX: ["0.55em", 0],
+    translateZ: 0,
+    rotateZ: [180, 0],
+    duration: 750,
+    easing: "easeOutExpo",
+    delay: (el, i) => 50 * i
+  }).add({
+    targets: '.ml7',
+    opacity: 0,
+    duration: 200,
+    easing: "easeOutExpo",
+    delay: 5000
+  });
 
     // Rotating Text
     const rotatingText = document.querySelector('.rotating-text');
